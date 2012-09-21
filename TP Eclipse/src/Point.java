e425949c92a1379f9c1efe7f7c7dd266fc92ebdd
marlenej <marlene.jaulin@eleves.ec-nantes.fr>
2012-09-21 10:48:21 +0200

    test MJA

---
+++ TP Eclipse/src/Point.java
@@ -0,0 +1,20 @@
+
+public class Point {
+
+	/** 
+	 * Abs du point
+	 */
+	protected int abs;
+
+	/** 
+	 * ord du point
+	 */
+	protected int ord;
+
+	/** 
+	 * constructeur de la classe point
+	 */
+	public Point(){
+		this.abs = 0;
+		this.ord = 0;}
+}
